97df32ef241b1a8ab0afaa18bed858468821c590
Jesus Abrahan Pena <jesusapm14@gmail.com>
2024-02-26 02:30:03 -0400

    refactor: :art: Improve structure by prettier & Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import 'react-native-gesture-handler';
-import { MMKV } from 'react-native-mmkv';
+import 'react-native-gesture-handler'
+import {MMKV} from 'react-native-mmkv'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-import { ThemeProvider } from '@/theme';
+import {ThemeProvider} from '@/theme'
 
-import ApplicationNavigator from './navigators/Root';
-import './translations';
+import ApplicationNavigator from './navigators/Root'
+import './translations'
 
-export const storage = new MMKV();
+export const storage = new MMKV()
+const queryClient = new QueryClient()
 
 function App() {
-	return (
-		<ThemeProvider storage={storage}>
-			<ApplicationNavigator />
-		</ThemeProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider storage={storage}>
+        <ApplicationNavigator />
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
--- src/screens/Example/Example.test.tsx
+++ src/screens/Example/Example.test.tsx
@@ -1,7 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react-native';
 import { MMKV } from 'react-native-mmkv';
 import { I18nextProvider } from 'react-i18next';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ThemeProvider } from '@/theme';
 import i18n from '@/translations';
@@ -10,17 +9,6 @@ import Example from './Example';
 
 describe('Example screen should render correctly', () => {
 	let storage: MMKV;
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				retry: false,
-				gcTime: Infinity,
-			},
-			mutations: {
-				gcTime: Infinity,
-			},
-		},
-	});
 
 	beforeAll(() => {
 		storage = new MMKV();
@@ -30,9 +18,7 @@ describe('Example screen should render correctly', () => {
 		const component = (
 			<ThemeProvider storage={storage}>
 				<I18nextProvider i18n={i18n}>
-					<QueryClientProvider client={queryClient}>
 						<Example />
-					</QueryClientProvider>
 				</I18nextProvider>
 			</ThemeProvider>
 		);
@@ -52,9 +38,7 @@ describe('Example screen should render correctly', () => {
 		const component = (
 			<ThemeProvider storage={storage}>
 				<I18nextProvider i18n={i18n}>
-					<QueryClientProvider client={queryClient}>
 						<Example />
-					</QueryClientProvider>
 				</I18nextProvider>
 			</ThemeProvider>
 		);
