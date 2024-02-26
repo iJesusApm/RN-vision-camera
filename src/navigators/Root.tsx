884f7c36f674cf21797eec231ab2e9e07f4b0dfa
Jesus Abrahan Pena <jesusapm14@gmail.com>
2024-02-26 02:30:32 -0400

    feat: :sparkles: add Permission screen

--- src/navigators/Root.tsx
+++ src/navigators/Root.tsx
@@ -1,24 +1,25 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native'
 
-import { Example, Startup } from '@/screens';
-import { useTheme } from '@/theme';
+import {Example, Startup, Permission} from '@/screens'
+import {useTheme} from '@/theme'
 
-import type { ApplicationStackParamList } from '@/types/navigation';
+import type {ApplicationStackParamList} from '@/types/navigation'
 
-const Stack = createStackNavigator<ApplicationStackParamList>();
+const Stack = createStackNavigator<ApplicationStackParamList>()
 
 function ApplicationNavigator() {
-	const { variant, navigationTheme } = useTheme();
+  const {variant, navigationTheme} = useTheme()
 
-	return (
-		<NavigationContainer theme={navigationTheme}>
-			<Stack.Navigator key={variant} screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="Startup" component={Startup} />
-				<Stack.Screen name="Example" component={Example} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <Stack.Navigator key={variant} initialRouteName='Permission' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Startup" component={Startup} />
+        <Stack.Screen name="Example" component={Example} />
+        <Stack.Screen name="Permission" component={Permission} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-export default ApplicationNavigator;
+export default ApplicationNavigator
---
+++ src/screens/Permission/Permission.tsx
@@ -0,0 +1,66 @@
+import {useNavigation} from '@react-navigation/native'
+import React, {useEffect, useState} from 'react'
+import {View, Text, TouchableOpacity, ScrollView, Linking} from 'react-native'
+
+import {Camera, CameraPermissionStatus} from 'react-native-vision-camera'
+
+import {Brand} from '@/components/molecules'
+import {SafeScreen} from '@/components/template'
+import {useTheme} from '@/theme'
+
+const PermissionsPage = () => {
+  const [cameraPermissionStatus, setCameraPermissionStatus] = useState<CameraPermissionStatus>('not-determined')
+  const {layout, gutters, fonts, components, backgrounds} = useTheme()
+
+  const navigation = useNavigation()
+
+  const requestCameraPermission = async () => {
+    console.log('Requesting camera permission...')
+    const permission = await Camera.requestCameraPermission()
+    console.log(`Camera permission status: ${permission}`)
+
+    if (permission === 'denied') await Linking.openSettings()
+    setCameraPermissionStatus(permission)
+  }
+
+  useEffect(() => {
+    if (cameraPermissionStatus === 'granted') {
+      console.log({cameraPermissionStatus})
+      // navigation.navigate('Camera')
+    }
+  }, [cameraPermissionStatus, navigation])
+
+  return (
+    <SafeScreen>
+      <ScrollView>
+        <View style={[layout.justifyCenter, layout.itemsCenter, gutters.marginTop_24]}>
+          <View style={[layout.relative, backgrounds.gray100, components.circle250]} />
+
+          <View style={[layout.absolute, gutters.paddingTop_40, gutters.paddingLeft_80]}>
+            <Brand height={300} width={500} />
+          </View>
+        </View>
+
+        <View style={[gutters.paddingHorizontal_32]}>
+          <View style={[gutters.marginTop_80]}>
+            <Text style={[fonts.size_40, fonts.gray800, fonts.bold]}>Welcome</Text>
+            <Text style={[fonts.gray400, fonts.bold, fonts.size_24, gutters.marginVertical_12]}>
+              The Vision Camera needs permission
+            </Text>
+          </View>
+
+          <View style={[layout.row, layout.justifyCenter, layout.fullWidth]}>
+            <TouchableOpacity
+              testID="grant-permission-button"
+              style={[components.buttonCircle, gutters.marginVertical_24]}
+              onPress={requestCameraPermission}>
+              <Text style={[fonts.size_16, fonts.gray800, fonts.bold, fonts.uppercase]}>Grant</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeScreen>
+  )
+}
+
+export default PermissionsPage
